41354d7e99fe448dc6d6244a62f2b3cc15a36a2c
BasD92 <BasD92@gmail.com>
2018-01-26 15:36:45 +0100

    change test.ts

--- dev/test.ts
+++ dev/test.ts
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        //console.log(myObj[0].speed);
+        console.log(myObj[0].speed);
         return myObj[0].speed;
     }
 };
